7036a81c718981c186ddd9ae331a9c86c7b71e19
Mamun <Mamun@UNIMAS.COM>
2023-03-13 18:57:19 +0600

    commit

--- script.js
+++ script.js
@@ -52,69 +52,141 @@ const restaurant = {
 // console.log(ingredients);
 // restaurant.orderPasta(...ingredients);
 
-const arr = [7, 8, 9];
-const newArr = [1, 2, ...arr];
-console.log(newArr);
+// const arr = [7, 8, 9];
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
 
-restaurant.orderDelivery({
-  time: '22.30',
-  address: 'Via del, 21',
-  mainIndex: 2,
-  starterIndex: 2,
-});
+// restaurant.orderDelivery({
+//   time: '22.30',
+//   address: 'Via del, 21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
 
-restaurant.orderDelivery({
-  address: 'Via del, 21',
-  mainIndex: 2,
-});
+// restaurant.orderDelivery({
+//   address: 'Via del, 21',
+//   mainIndex: 2,
+// });
 
-const [starter, mainCourse] = restaurant.order(1, 1);
+// const [starter, mainCourse] = restaurant.order(1, 1);
 
-const { name, categories, mainMenu } = restaurant;
-const { name: newName, categories: newCategories, mainMenu: Menu } = restaurant;
-console.log(name, categories);
-console.log(newName, newCategories, Menu);
+// const { name, categories, mainMenu } = restaurant;
+// const { name: newName, categories: newCategories, mainMenu: Menu } = restaurant;
+// console.log(name, categories);
+// console.log(newName, newCategories, Menu);
 
-console.log(openingHours);
+// console.log(openingHours);
 
-const {
-  fri: { open: o, close: c },
-} = openingHours;
-// console.log(open, close);
-console.log(o, c);
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// // console.log(open, close);
+// console.log(o, c);
 
-console.log(3 || 'Jonas');
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
-console.log(undefined || 0 || '' || 'hello' || 23 || null);
+// console.log(3 || 'Jonas');
+// console.log('' || 'Jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
+// console.log(undefined || 0 || '' || 'hello' || 23 || null);
 
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
 
-const a = ['a', 'b', 1, 6, 'th', null];
+// const a = ['a', 'b', 1, 6, 'th', null];
 
-const reduce = a.reduce(
-  (acc, v) => {
-    if (typeof v === 'string') acc.string.push(v);
-    if (typeof v === 'number') acc.number = acc.number + v;
-    if (typeof v !== 'number' && typeof v !== 'string')
-      acc.other = acc.other + v;
+// const reduce = a.reduce(
+//   (acc, v) => {
+//     if (typeof v === 'string') acc.string.push(v);
+//     if (typeof v === 'number') acc.number = acc.number + v;
+//     if (typeof v !== 'number' && typeof v !== 'string')
+//       acc.other = acc.other + v;
 
-    return acc;
+//     return acc;
+//   },
+//   { string: [], number: 0, other: '' }
+// );
+// console.log(reduce);
+
+// const Reverse = arr => {
+//   // limit specifies the amount of Reverse actions
+//   for (let i = 0, j = arr.length - 1; i < arr.length / 2; i++, j--) {
+//     const temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//   }
+//   return arr;
+// };
+
+// console.log(Reverse([2, 3, 9]));
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
-  { string: [], number: 0, other: '' }
-);
-console.log(reduce);
-
-const Reverse = arr => {
-  // limit specifies the amount of Reverse actions
-  for (let i = 0, j = arr.length - 1; i < arr.length / 2; i++, j--) {
-    const temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
-  return arr;
 };
 
-console.log(Reverse([2, 3, 9]));
+const [players1, players2] = game.players;
+// console.log(players1);
+// console.log(players2);
+
+const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = ['Thiago', 'Coutinho', 'Perisic', ...players1];
+console.log(players1Final);
+
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
