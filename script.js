e1f5500357a10bc1aceaf355ca814242eca50625
Mamun <Mamun@UNIMAS.COM>
2023-03-20 18:41:33 +0600

    commit

--- script.js
+++ script.js
@@ -44,9 +44,9 @@ const restaurant = {
     console.log(`Here is your pasta with ${ing1},${ing2} and ${ing3}`);
   },
 };
-for (const day of Object.keys(openingHours)) {
-  console.log(day);
-}
+// for (const day of Object.keys(openingHours)) {
+//   console.log(day);
+// }
 // const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
 // for (const item of menu) console.log(item);
 
@@ -207,13 +207,13 @@ const game = {
     team2: 6.5,
   },
 };
-for (const [i, el] of game.scored.entries()) {
-  console.log(`Goal ${i + 1}: ${el}`);
-}
-const scorers = {};
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
-}
+// for (const [i, el] of game.scored.entries()) {
+//   console.log(`Goal ${i + 1}: ${el}`);
+// }
+// const scorers = {};
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+// }
 //1
 // const [players1, players2] = game.players;
 // // console.log(players1);
@@ -242,7 +242,7 @@ for (const player of game.scored) {
 
 const dog = new Set(['Milky', 'Mamun', 'Milky', 'Protik']);
 
-console.log(dog);
+// console.log(dog);
 
 // const bookings = [];
 // const createBooking = function (flightName, flightTime = 1, price = 100) {
@@ -277,11 +277,11 @@ const mamun2 = {
 // console.log(flight);
 // console.log(mamun);
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 1000000);
-};
-newPassport(mamun);
-console.log(mamun);
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000);
+// };
+// newPassport(mamun);
+// console.log(mamun);
 
 const data = {
   name: 'John Doe',
@@ -334,3 +334,135 @@ const data = {
     },
   ],
 };
+
+const orders = [
+  {
+    id: 'order-1',
+    customer: 'Alice',
+    items: [
+      { id: 'item-1', name: 'Widget', price: 9.99, quantity: 2 },
+      { id: 'item-2', name: 'Gizmo', price: 4.99, quantity: 3 },
+    ],
+  },
+  {
+    id: 'order-2',
+    customer: 'Bob',
+    items: [
+      { id: 'item-3', name: 'Thingamajig', price: 14.99, quantity: 1 },
+      { id: 'item-4', name: 'Whatchamacallit', price: 7.99, quantity: 2 },
+    ],
+  },
+  {
+    id: 'order-3',
+    customer: 'Charlie',
+    items: [
+      { id: 'item-5', name: 'Doodad', price: 2.99, quantity: 5 },
+      { id: 'item-6', name: 'Doohickey', price: 1.99, quantity: 10 },
+    ],
+  },
+];
+
+// orders.forEach(order => {
+//   order.items.forEach(item => {
+//     item.totalSell = Math.trunc(item.price * item.quantity);
+//   });
+// });
+// console.log(orders);
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greeterHey = greet('Hey');
+// greeterHey('Jonas');
+// greeterHey('Mamun');
+
+// const greet2 = greeting2 => name2 => console.log(`${greeting2} ${name2}`);
+// const greeterHey2 = greet2('Hey');
+// greeterHey2('Mamun2');
+
+// const funn = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} to ${name}`);
+//   };
+// };
+
+// const faru = funn('Welcome')('Mamun');
+
+const address = {
+  name: 'Mamun',
+  age: 23,
+  address: 'Gazipur',
+  details(name, address) {
+    return console.log(
+      `${name} is ${this.age} years old and leave in ${address}`
+    );
+  },
+};
+
+address.details('Mamun', 'Gazipur');
+
+const address2 = {
+  age: 40,
+};
+const newArr = ['Milky', 'Joshour'];
+address.details.bind(address2, ...newArr);
+
+address.fruits = 300;
+address.count = function () {
+  console.log(this);
+  this.fruits++;
+  console.log(this.fruits);
+};
+address.count();
+
+const btn = document.querySelector('.this');
+
+btn.addEventListener('click', address.count.bind(address));
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+const poll = {
+  quession: 'What is the most popular programing language?',
+  options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+  },
+};
+const polling = document.querySelector('.poll');
+polling.addEventListener('click', poll.registerNewAnswer);
+console.log(poll);
